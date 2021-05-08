5845a53e0b97979721d4fb6bb243dab4eaada209
Eduard van Valkenburg <github@vanvalkenburg.eu>
2021-05-08 11:34:47 +0000

    new version of config_entry with migration and fix for #45

--- custom_components/sia/__init__.py
+++ custom_components/sia/__init__.py
@@ -1,5 +1,6 @@
 """The sia integration."""
 import asyncio
+import logging
 
 from homeassistant.components.alarm_control_panel import (
     DOMAIN as ALARM_CONTROL_PANEL_DOMAIN,
@@ -7,7 +8,7 @@ from homeassistant.components.alarm_control_panel import (
 from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PORT
+from homeassistant.const import CONF_PORT, CONF_PROTOCOL
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
@@ -15,6 +16,8 @@ from .const import DOMAIN
 from .hub import SIAHub
 
 PLATFORMS = [ALARM_CONTROL_PANEL_DOMAIN, BINARY_SENSOR_DOMAIN, SENSOR_DOMAIN]
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
@@ -57,3 +60,17 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
         hub: SIAHub = hass.data[DOMAIN].pop(entry.entry_id)
         await hub.async_shutdown()
     return unload_ok
+
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Migrate old entry."""
+    _LOGGER.debug("Migrating from version %s", config_entry.version)
+
+    if config_entry.version == 1:
+        data = config_entry.data.copy()
+        data[CONF_PROTOCOL] = "TCP"
+        config_entry.version = 2
+        hass.config_entries.async_update_entry(config_entry, data=data)
+
+    _LOGGER.info("Migration to version %s successful", config_entry.version)
+    return True
--- custom_components/sia/config_flow.py
+++ custom_components/sia/config_flow.py
@@ -1,10 +1,6 @@
 """Config flow for sia integration."""
 import logging
 
-import voluptuous as vol
-from homeassistant import config_entries, exceptions
-from homeassistant.const import CONF_PORT
-from homeassistant.data_entry_flow import AbortFlow
 from pysiaalarm import (
     InvalidAccountFormatError,
     InvalidAccountLengthError,
@@ -12,12 +8,18 @@ from pysiaalarm import (
     InvalidKeyLengthError,
     SIAAccount,
 )
+import voluptuous as vol
+
+from homeassistant import config_entries, exceptions
+from homeassistant.const import CONF_PORT, CONF_PROTOCOL
+from homeassistant.data_entry_flow import AbortFlow
 
 from .const import (
     CONF_ACCOUNT,
     CONF_ACCOUNTS,
     CONF_ADDITIONAL_ACCOUNTS,
     CONF_ENCRYPTION_KEY,
+    CONF_IGNORE_TIMESTAMPS,
     CONF_PING_INTERVAL,
     CONF_ZONES,
     DOMAIN,
@@ -29,10 +31,12 @@ _LOGGER = logging.getLogger(__name__)
 HUB_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_PORT): int,
+        vol.Optional(CONF_PROTOCOL, default="TCP"): vol.In(["TCP", "UDP"]),
         vol.Required(CONF_ACCOUNT): str,
         vol.Optional(CONF_ENCRYPTION_KEY): str,
         vol.Required(CONF_PING_INTERVAL, default=1): int,
         vol.Required(CONF_ZONES, default=1): int,
+        vol.Required(CONF_IGNORE_TIMESTAMPS, default=False): bool,
         vol.Optional(CONF_ADDITIONAL_ACCOUNTS, default=False): bool,
     }
 )
@@ -43,6 +47,7 @@ ACCOUNT_SCHEMA = vol.Schema(
         vol.Optional(CONF_ENCRYPTION_KEY): str,
         vol.Required(CONF_PING_INTERVAL, default=1): int,
         vol.Required(CONF_ZONES, default=1): int,
+        vol.Required(CONF_IGNORE_TIMESTAMPS, default=False): bool,
         vol.Optional(CONF_ADDITIONAL_ACCOUNTS, default=False): bool,
     }
 )
@@ -55,105 +60,95 @@ def validate_input(data: dict):
     try:
         ping = int(data[CONF_PING_INTERVAL])
         assert 1 <= ping <= 1440
-    except AssertionError:
-        raise InvalidPing
+    except AssertionError as invalid_ping:
+        raise InvalidPing from invalid_ping
     try:
         zones = int(data[CONF_ZONES])
         assert zones > 0
-    except AssertionError:
-        raise InvalidZones
+    except AssertionError as invalid_zone:
+        raise InvalidZones from invalid_zone
 
 
 class SIAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for sia."""
 
-    VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
-    data = None
+    VERSION = 2
+
+    def __init__(self):
+        """Initialize the config flow."""
+        self._data = {}
 
     async def async_step_add_account(self, user_input: dict = None):
         """Handle the additional accounts steps."""
-        errors = {}
-        if user_input is not None:
-            try:
-                validate_input(user_input)
-                self.update_data(user_input)
-                if user_input[CONF_ADDITIONAL_ACCOUNTS]:
-                    return await self.async_step_add_account()
-            except InvalidKeyFormatError:
-                errors["base"] = "invalid_key_format"
-            except InvalidKeyLengthError:
-                errors["base"] = "invalid_key_length"
-            except InvalidAccountFormatError:
-                errors["base"] = "invalid_account_format"
-            except InvalidAccountLengthError:
-                errors["base"] = "invalid_account_length"
-            except InvalidPing:
-                errors["base"] = "invalid_ping"
-            except InvalidZones:
-                errors["base"] = "invalid_zones"
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=ACCOUNT_SCHEMA,
-            errors=errors,
-        )
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user",
+                data_schema=ACCOUNT_SCHEMA,
+                errors={},
+            )
 
     async def async_step_user(self, user_input: dict = None):
         """Handle the initial step."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=HUB_SCHEMA, errors={}
+            )
         errors = {}
-        if user_input is not None:
-            try:
-                validate_input(user_input)
-                await self.async_set_unique_id(f"{DOMAIN}_{self.data[CONF_PORT]}")
-                self._abort_if_unique_id_configured()
-                self.update_data(user_input)
-                if not user_input[CONF_ADDITIONAL_ACCOUNTS]:
-                    return self.async_create_entry(
-                        title=f"SIA Alarm on port {self.data[CONF_PORT]}",
-                        data=self.data,
-                    )
-                return await self.async_step_add_account()
-            except InvalidKeyFormatError:
-                errors["base"] = "invalid_key_format"
-            except InvalidKeyLengthError:
-                errors["base"] = "invalid_key_length"
-            except InvalidAccountFormatError:
-                errors["base"] = "invalid_account_format"
-            except InvalidAccountLengthError:
-                errors["base"] = "invalid_account_length"
-            except InvalidPing:
-                errors["base"] = "invalid_ping"
-            except InvalidZones:
-                errors["base"] = "invalid_zones"
-            except AbortFlow:
-                return self.async_abort(reason="already_configured")
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+        try:
+            validate_input(user_input)
+        except InvalidKeyFormatError:
+            errors["base"] = "invalid_key_format"
+        except InvalidKeyLengthError:
+            errors["base"] = "invalid_key_length"
+        except InvalidAccountFormatError:
+            errors["base"] = "invalid_account_format"
+        except InvalidAccountLengthError:
+            errors["base"] = "invalid_account_length"
+        except InvalidPing:
+            errors["base"] = "invalid_ping"
+        except InvalidZones:
+            errors["base"] = "invalid_zones"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        if errors:
+            return self.async_show_form(
+                step_id="user", data_schema=HUB_SCHEMA, errors=errors
+            )
+        self.update_data(user_input)
+        await self.async_set_unique_id(f"{DOMAIN}_{self._data[CONF_PORT]}")
+        try:
+            self._abort_if_unique_id_configured()
+        except AbortFlow:
+            return self.async_abort(reason="already_configured")
 
-        return self.async_show_form(
-            step_id="user", data_schema=HUB_SCHEMA, errors=errors
+        if user_input[CONF_ADDITIONAL_ACCOUNTS]:
+            return await self.async_step_add_account()
+        return self.async_create_entry(
+            title=f"SIA Alarm on port {self._data[CONF_PORT]}",
+            data=self._data,
         )
 
     def update_data(self, user_input):
         """Parse the user_input and store in self.data."""
-        if not self.data:
-            self.data = {
+        if not self._data:
+            self._data = {
                 CONF_PORT: user_input[CONF_PORT],
+                CONF_PROTOCOL: user_input[CONF_PROTOCOL],
                 CONF_ACCOUNTS: [
                     {
                         CONF_ACCOUNT: user_input[CONF_ACCOUNT],
                         CONF_ENCRYPTION_KEY: user_input.get(CONF_ENCRYPTION_KEY),
                         CONF_PING_INTERVAL: user_input[CONF_PING_INTERVAL],
                         CONF_ZONES: user_input[CONF_ZONES],
+                        CONF_IGNORE_TIMESTAMPS: user_input[CONF_IGNORE_TIMESTAMPS],
                     }
                 ],
             }
         else:
             add_data = user_input.copy()
             add_data.pop(CONF_ADDITIONAL_ACCOUNTS)
-            self.data[CONF_ACCOUNTS].append(add_data)
+            self._data[CONF_ACCOUNTS].append(add_data)
 
 
 class InvalidPing(exceptions.HomeAssistantError):
